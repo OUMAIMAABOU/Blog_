dedfd78df75952e288e17727ee02a659c53e5aa9
oumaima <oumaimaabouelhaitam@gmail.com>
2023-11-07 20:58:03 +0100

    connect to the  database

--- service.js
+++ service.js
@@ -1,1 +1,9 @@
 require("dotenv").config();
+const express = require("express");
+const mongoose = require("./Config/db");
+const app = express();
+app.use(express.json());
+app.listen(process.env.PORT || 8082, () => {
+  console.log(`https://localhost:${process.env.PORT}`);
+});
+module.exports = app;
